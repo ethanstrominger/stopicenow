b1ff082a309af01bb432c47c6df3daaca33192b1
Ethan Strominger <ethanstrominger2@gmail.com>
2026-01-27 09:08:44 -0500

    Color adjustment

--- dist/title.js
+++ dist/title.js
@@ -6,7 +6,7 @@ function insertSiteTitle() {
   const container = document.getElementById('site-title-container');
   if (!container) return;
   container.innerHTML = `
-    <div style="text-align:center; background:#fff;">
+    <div style="text-align:center; background:#fff; margin-bottom:0.5em;">
       <h1 style="font-size:2.5em; color:#b30000; font-weight:900; margin-bottom:0.1em; margin-top:0; letter-spacing:0.01em;">FIX ICE</h1>
       <div style="font-size:1.35em; color:#005580; font-weight:600; margin-bottom:0.7em;">Burlington, MA and Beyond</div>
     </div>
